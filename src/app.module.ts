3586bfabfb31c4201d7fedc6f5734ec8d51c4560
Neil David Sanchez Quintana <nd.sanchezq@gmail.com>
2023-05-08 23:47:07 -0500

    database setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { db } from './config/db';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(db)],
   controllers: [AppController],
   providers: [AppService],
 })
